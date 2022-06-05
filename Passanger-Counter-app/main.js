c277c08e4054c11369df55d6b83f5f0ccd2074c8
JulioCMR <80418425+JulioCMR@users.noreply.github.com>
2022-06-04 20:47:28 -0500

    Added Reassigning & incrementing/decreasing exercises and code

--- Passanger-Counter-app/main.js
+++ Passanger-Counter-app/main.js
@@ -4,7 +4,6 @@
 
 let count = 3
 
-console.log(count)
 
 // 1. Create a variable, myAge, and set it's value to your age
 // 2. Log the myAge variable to the console
@@ -30,5 +29,33 @@ let myDogAge = myAge * humanDogRatio
 
 console.log(myDogAge)
 
+// ~~~~~ Reassigning & incrementing/decreasing ~~~~~
+
+//There are two ways to do this, the first is the classic, you mention again the variable and if you want to increment or decrease the value
+
+let conteo = 5
+
+conteo = conteo + 10 // equal to 15
+
+//And my favorite, I don't how is called, but for me is the easy way
+
+conteo += 20 //Equal to 35 (the past 10 value were counted)
 
 
+// 1. Create a variable. bonusPoints. Initialize it as 50. Increase it to 100.
+// 2. Decrease it down to 25, and then finally increase it to 70.
+// Console.log the value after each step
+
+let bonusPoints = 50
+
+bonusPoints += 50
+
+console.log(bonusPoints)
+
+bonusPoints -= 75
+
+console.log(bonusPoints)
+
+bonusPoints += 45
+
+console.log(bonusPoints)
