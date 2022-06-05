62e1cf2c4a6f84f0a89ad11c119c0aabcaafa7a6
JulioCMR <80418425+JulioCMR@users.noreply.github.com>
2022-06-04 20:31:39 -0500

    Added Mathematical Operations

--- Passanger-Counter-app/main.js
+++ Passanger-Counter-app/main.js
@@ -7,10 +7,28 @@ let count = 3
 console.log(count)
 
 // 1. Create a variable, myAge, and set it's value to your age
+// 2. Log the myAge variable to the console
 
 let myAge = 27
 
-// 2. Log the myAge variable to the console
-
 console.log(myAge)
+
+// ~~~~~ Mathematical Operations ~~~~~~
+
+// +, -, *, /; You can do whatever operation you want, for example:
+
+let add = 5 + 6 //in console.log(add), is equal to 11
+
+// 1. Create two variable, myAge and humanDogRatio
+// 2. Multiply the two together and store the result in myDogAge
+// 3. Log myDogAge to the console
+
+//myAge is already created.
+let humanDogRatio = 7
+
+let myDogAge = myAge * humanDogRatio
+
+console.log(myDogAge)
+
+
 
