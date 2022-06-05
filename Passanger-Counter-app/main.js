17326856205a276ac327551b533d511fc1acb255
JulioCMR <80418425+JulioCMR@users.noreply.github.com>
2022-06-04 20:11:10 -0500

    Added introduction to variables

--- Passanger-Counter-app/main.js
+++ Passanger-Counter-app/main.js
@@ -1,3 +1,16 @@
 // JavaScript File for Passanger Counting file
 
-let count = 5
+// ~~~~~ Variables ~~~~~
+
+let count = 3
+
+console.log(count)
+
+// 1. Create a variable, myAge, and set it's value to your age
+
+let myAge = 27
+
+// 2. Log the myAge variable to the console
+
+console.log(myAge)
+
