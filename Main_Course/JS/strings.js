533cc5419a899c7c342e510281bfc3bcd9e6267b
JulioCMR <80418425+JulioCMR@users.noreply.github.com>
2022-06-27 14:56:58 -0500

    Added JavaScript Directory to Main_Course

---
+++ Main_Course/JS/strings.js
@@ -0,0 +1,45 @@
+/* What is a string?
+It's a Data type that represents text. They consist of a sequence of characters inside single or double quotes. */
+
+/* What will hapenn if  you concatenate a string and a number?
+The number will be converted to a string and it will concatenated with the string.
+
+Example : let conca = "4" + 45 will be 445 */
+
+// Exercises
+// 1. Create a variable, message, that stores the string = "You have three new notificacions".
+
+let message = "You have three new notifications"
+
+
+
+// 2. Create a variable, messagetoUser, that contains a complete concatenated message (with the username).
+
+let username = "Julio"
+
+let messagetoUser = message + ", " + username + "!"
+
+// 3. Create two variables, name and greeting. The name variable should store your name, and the greeting should store e.g "Hi, my name is "
+
+let name = "Julio"
+
+let greeting = "Hi, my name is "
+
+// 4. Create a variable, myGreeting, that concatenates the two strings, and log myGreeting to the console.
+
+let myGreeting = greeting + name
+
+console.log(myGreeting)
+
+// 5. Create two variables, num and age, and concatenate them in one variable, numstr.
+
+let num = 28
+
+let age = "28"
+
+let numstr = num + age
+
+console.log(numstr)
+
+
+
---
+++ Main_Course/JS/variables.js
@@ -0,0 +1,61 @@
+
+
+// ~~~~~ Variables ~~~~~
+
+let count = 3
+
+
+// 1. Create a variable, myAge, and set it's value to your age
+// 2. Log the myAge variable to the console
+
+let myAge = 27
+
+console.log(myAge)
+
+// ~~~~~ Mathematical Operations ~~~~~~
+
+// +, -, *, /; You can do whatever operation you want, for example:
+
+let add = 5 + 6 //in console.log(add), is equal to 11
+
+// 1. Create two variable, myAge and humanDogRatio
+// 2. Multiply the two together and store the result in myDogAge
+// 3. Log myDogAge to the console
+
+//myAge is already created.
+let humanDogRatio = 7
+
+let myDogAge = myAge * humanDogRatio
+
+console.log(myDogAge)
+
+// ~~~~~ Reassigning & incrementing/decreasing ~~~~~
+
+//There are two ways to do this, the first is the classic, you mention again the variable and if you want to increment or decrease the value
+
+let conteo = 5
+
+conteo = conteo + 10 // equal to 15
+
+//And my favorite, I don't how is called, but for me is the easy way
+
+conteo += 20 //Equal to 35 (the past 10 value were counted)
+
+
+// 1. Create a variable. bonusPoints. Initialize it as 50. Increase it to 100.
+// 2. Decrease it down to 25, and then finally increase it to 70.
+// Console.log the value after each step
+
+let bonusPoints = 50
+
+bonusPoints += 50
+
+console.log(bonusPoints)
+
+bonusPoints -= 75
+
+console.log(bonusPoints)
+
+bonusPoints += 45
+
+console.log(bonusPoints)
