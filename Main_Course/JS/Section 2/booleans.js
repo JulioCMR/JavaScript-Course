2131b3c5758fcedaf62ed043fb93b599f046a337
JulioCMR <80418425+JulioCMR@users.noreply.github.com>
2022-07-13 16:21:32 -0500

    Added boolean.js

---
+++ Main_Course/JS/Section 2/booleans.js
@@ -0,0 +1,17 @@
+// Questions -----
+
+// What are Booleans?
+
+// They are a type of data that are represent by to values: false and true. They are the heart of the conditionals
+
+// Exercises -----
+
+// Check the boolean value resulted from these conditionals
+
+console.log(4 === 3) // false
+console.log(5 > 2) // true
+console.log(12 > 12) // false
+console.log(3 < 0) // false
+console.log(3 >= 3) // true
+console.log(11 <= 11) // true
+console.log(3 <= 2) // false
