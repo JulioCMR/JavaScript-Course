b90ec53d3fb2b4eaec07134e381ac19293909683
JulioCMR <80418425+JulioCMR@users.noreply.github.com>
2022-07-13 17:32:13 -0500

    Added button

--- BlackJack/main.js
+++ BlackJack/main.js
@@ -7,22 +7,26 @@ let firstCard = 4
 let secondCard = 6
 let hasBlackjack = false
 let isAlive = true
+let message = ""
 
-let sum = firstCard + secondCard 
+let sum = firstCard + secondCard + 11
 
-// Conditionals (if-else) ---
+// Conditionals (if-else and booleans) ---
 
+function startGame() {
 
-if (sum <= 20) {
-    console.log("Do you want to draw a new card?🙂")
-} else if (sum === 21) {
-    console.log("Wohoo! You've got Blackjack!🤑")
-    hasBlackjack = true
-} else {
-    console.log("You're out of the game🤡")
-    isAlive = false
+    if (sum <= 20) {
+        message = "Do you want to draw a new card?🙂"
+    } else if (sum === 21) {
+        message = "Wohoo! You've got Blackjack!🤑"
+        hasBlackjack = true
+    } else {
+        message = "You're out of the game🤡"
+        isAlive = false
+    }
+
+    console.log(message)
 }
 
-// Conditionals (Booleans)
-console.log(hasBlackjack)
-console.log(isAlive)
+
+
